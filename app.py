22fa1db09cc1b8dcbecd4de70ca3a0d456cbab66
James <85806019+James-Discord@users.noreply.github.com>
2023-10-09 10:46:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import requests
 from flask import Flask, request
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
-# Set your OpenAI API key and base URL
+# Set your Nova AI API key
 nova_api_key = 'NOVA_AI_KEY'
 
-# Function to extract video ID from a YouTube URL
 def extract_video_id(url):
     parts = url.split("?")
     if len(parts) > 1:
@@ -19,7 +20,6 @@ def extract_video_id(url):
                 return value
     return None
 
-# Function to get transcript for a video using its ID
 def get_youtube_transcript(video_url):
     video_id = extract_video_id(video_url)
 
@@ -28,25 +28,26 @@ def get_youtube_transcript(video_url):
             transcript = YouTubeTranscriptApi.get_transcript(video_id)
             return transcript
         except Exception as e:
-            print(f"Error fetching transcript: {str(e)}")
+            app.logger.error(f"Error fetching transcript: {str(e)}")
             return None
     else:
         return None
 
-# Function to get video title using pytube
-def get_video_title(video_url):
+def get_video_details(video_url):
     try:
         yt = YouTube(video_url)
-        return yt.title
+        return yt.title, yt.author
     except Exception as e:
-        print(f"Error fetching video title: {str(e)}")
-        return None
+        app.logger.error(f"Error fetching video details: {str(e)}")
+        return None, None
 
-# Function to summarize text using Nova AI with a custom system prompt
-def summarize_text_with_nova_ai(video_title, text):
+def summarize_text_with_nova_ai(video_author, video_title, text):
     if text:
-        system_prompt = f"Title: **{video_title}**\n" + (
+        system_prompt = (
+            f"YouTuber: **{video_author}**\n"
+            f"Title: **{video_title}**\n"
             "Your output should use the following template:\n"
+            "### YouTuber\n"
             "### Title\n"
             "### Summary\n"
             "### Highlights\n"
@@ -54,14 +55,17 @@ def summarize_text_with_nova_ai(video_title, text):
             "Your task is to summarize the text I have given you in up to seven concise bullet points, starting with a short highlight. Choose an appropriate emoji for each bullet point."
         )
     else:
-        system_prompt = f"Title: **{video_title}**\n" + (
-            "Your output should use the following template:\n"
-            "### Title\n"
-            "### Summary\n"
-            "####Highlights\n"
-            "- [Emoji] Bulletpoint\n\n"
-            "Your task is to summarize the video title with the information you know. I have given you in up to seven concise bullet points, starting with a short highlight. Choose an appropriate emoji for each bullet point."
-        )
+        system_prompt = (
+            f"YouTuber: **{video_author}**\n"
+            f"Title: **{video_title}**\n"
+        "Your output should use the following template:\n"
+        "### YouTuber\n"
+        "### Title\n"
+        "### Summary\n"
+        "### Highlights\n"
+        "- [Emoji] Bulletpoint\n\n"
+        "Your task is to summarize the video title with the information you know. I have given you in up to seven concise bullet points, starting with a short highlight. Choose an appropriate emoji for each bullet point."
+    )
 
     nova_api_url = 'https://api.nova-oss.com/v1/chat/completions'
     data = {
@@ -74,11 +78,10 @@ def summarize_text_with_nova_ai(video_title, text):
         ]
     }
 
-    # Include the user message content only if there's text available
     if text:
         data["messages"].append({
             "role": "user",
-            "content": f"{video_title} {text}"
+            "content": f"{text}"
         })
 
     headers = {
@@ -87,31 +90,36 @@ def summarize_text_with_nova_ai(video_title, text):
     }
     response = requests.post(nova_api_url, json=data, headers=headers)
 
-    print("Nova AI API Response:", response.json())  # Print API response to the console
-
     if response.status_code == 200:
         response_data = response.json()
         if 'choices' in response_data:
             assistant_message = response_data['choices'][0]['message']
-            completion_message = assistant_message['content']
-            return completion_message
+            if 'content' in assistant_message:
+                completion_message = assistant_message['content']
+                return completion_message
 
     return video_title  # Return video title if no summary is available
 
-@app.route('/', methods=['POST'])
-def index():
+@app.route('/api/v1/summarize', methods=['POST'])
+def summarize():
     youtube_url = request.form.get('youtube_url')
     transcript = get_youtube_transcript(youtube_url)
-    video_title = get_video_title(youtube_url)
+    video_title, video_author = get_video_details(youtube_url)
 
     if transcript:
         # Extract text from the transcript
         transcript_text = ' '.join([entry['text'] for entry in transcript])
-        summary = summarize_text_with_nova_ai(video_title, transcript_text)
+        summary = summarize_text_with_nova_ai(video_author, video_title, transcript_text)
     else:
-        summary = summarize_text_with_nova_ai(video_title, None)
+        summary = summarize_text_with_nova_ai(video_author, video_title, None)
 
     return summary
 
 if __name__ == "__main__":
+    handler = RotatingFileHandler('transcript.log', maxBytes=10000, backupCount=3)
+    formatter = logging.Formatter(
+        "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
     app.run(host='0.0.0.0', debug=True)
